30287fe020b159d2badb549a8e86d6b61803fa3a
EunJin Lee <dldmswls0910@naver.com>
2023-03-18 22:13:01 +0900

    commit intro 타이핑 효과

--- chart.js
+++ chart.js
@@ -1,132 +1,138 @@
-const chart = document.getElementById("myChart");
-const ctx = document.getElementById("myChart").getContext("2d");
+window.addEventListener("load", onload);
+function onload() {
+    makeMyChart();
+    makeChartSkills();
+}
 
-const myChart = new Chart(ctx, {
-    type: "radar",
-    data: {
-        labels: ["외향성(E)", "감각(S)", "느낌(F)", "판단(J)", "내향성(I)", "직감(N)", "생각(T)", "인식(P)"],
-        datasets: [
-            {
-                data: [39, 62, 36, 69, 61, 38, 64, 31],
-                backgroundColor: ["rgba(176, 157, 203, 0.1)"],
-                borderColor: ["rgba(176, 157, 203, 1)"],
-                borderWidth: 1,
-                pointStyle: "circle",
-                pointRadius: [3, 5, 3, 5, 5, 3, 5, 3],
-                pointBackgroundColor: ["rgba(176, 157, 203, 1)"],
-            },
-        ],
-    },
-    options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                display: false,
-            },
-            tooltip: {
-                usePointStyle: true,
-            },
+function makeMyChart() {
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const myChart = new Chart(ctx, {
+        type: "radar",
+        data: {
+            labels: ["외향성(E)", "감각(S)", "느낌(F)", "판단(J)", "내향성(I)", "직감(N)", "생각(T)", "인식(P)"],
+            datasets: [
+                {
+                    data: [39, 62, 36, 69, 61, 38, 64, 31],
+                    backgroundColor: ["rgba(176, 157, 203, 0.1)"],
+                    borderColor: ["rgba(176, 157, 203, 1)"],
+                    borderWidth: 1,
+                    pointStyle: "circle",
+                    pointRadius: [3, 5, 3, 5, 5, 3, 5, 3],
+                    pointBackgroundColor: ["rgba(176, 157, 203, 1)"],
+                },
+            ],
         },
-        scales: {
-            r: {
-                min: 0,
-                max: 100,
-                ticks: {
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
                     display: false,
                 },
-                pointLabels: {
-                    font: {
-                        family: 'NanumSquareNeo-Variable',
-                        size: function (context) {
-                            var index = context.index;
-                            if (index === 1 || index === 3 || index === 4 || index === 6) {
-                                return 15;
-                            } else {
-                                return 13;
-                            }
-                        },
-                        weight: function (context) {
-                            var index = context.index;
-                            if (index === 1 || index === 3 || index === 4 || index === 6) {
-                                return "bold";
-                            } else {
-                                return "normal";
-                            }
-                        },
+                tooltip: {
+                    usePointStyle: true,
+                },
+            },
+            scales: {
+                r: {
+                    min: 0,
+                    max: 100,
+                    ticks: {
+                        display: false,
                     },
-                    color: function (context) {
-                        var index = context.index;
-                        if (index === 1 || index === 3 || index === 4 || index === 6) {
-                            return "#7272B7";
-                        } else {
-                            return "black";
-                        }
+                    pointLabels: {
+                        font: {
+                            family: 'NanumSquareNeo-Variable',
+                            size: function (context) {
+                                var index = context.index;
+                                if (index === 1 || index === 3 || index === 4 || index === 6) {
+                                    return 15;
+                                } else {
+                                    return 13;
+                                }
+                            },
+                            weight: function (context) {
+                                var index = context.index;
+                                if (index === 1 || index === 3 || index === 4 || index === 6) {
+                                    return "bold";
+                                } else {
+                                    return "normal";
+                                }
+                            },
+                        },
+                        color: function (context) {
+                            var index = context.index;
+                            if (index === 1 || index === 3 || index === 4 || index === 6) {
+                                return "#7272B7";
+                            } else {
+                                return "black";
+                            }
+                        },
                     },
                 },
             },
-        },
-        ticks: {
-            beginAtZero: true,
-            stepSize: 50,
-        },
-    },
-});
-
-const chart2 = document.getElementById("chart-skills");
-const ctx2 = document.getElementById("chart-skills").getContext("2d");
-const myChart2 = new Chart(ctx2, {
-    type: "bar",
-    data: {
-        labels: ["Java", "MySQL", "HTML5", "CSS3", "Javascript", "GitHub"],
-        datasets: [
-            {
-                data: [65, 50, 55, 45, 45, 40],
-                backgroundColor: [
-                    "rgba(176, 157, 203, 0.8)",
-                    "rgba(176, 157, 203, 0.5)",
-                    "rgba(176, 157, 203, 0.7)",
-                    "rgba(176, 157, 203, 0.5)",
-                    "rgba(176, 157, 203, 0.5)",
-                    "rgba(176, 157, 203, 0.4)"
-                ],
-                borderColor: ["rgba(176, 157, 203, 1)"],
-                borderWidth: 1,
-                borderRadius: 3
+            ticks: {
+                beginAtZero: true,
+                stepSize: 50,
             },
-        ],
-    },
-    options: {
-        indexAxis: 'y',
-        plugins: {
-            legend: {
-                display: false,
-            }
         },
-        scales: {
-            r: {
-                ticks: {
+    });
+}
+
+function makeChartSkills() {
+    const ctx2 = document.getElementById("chart-skills").getContext("2d");
+    const myChart2 = new Chart(ctx2, {
+        type: "bar",
+        data: {
+            labels: ["Java", "MySQL", "HTML5", "CSS3", "Javascript", "GitHub"],
+            datasets: [
+                {
+                    data: [65, 50, 55, 45, 45, 40],
+                    backgroundColor: [
+                        "rgba(176, 157, 203, 0.8)",
+                        "rgba(176, 157, 203, 0.5)",
+                        "rgba(176, 157, 203, 0.7)",
+                        "rgba(176, 157, 203, 0.5)",
+                        "rgba(176, 157, 203, 0.5)",
+                        "rgba(176, 157, 203, 0.4)"
+                    ],
+                    borderColor: "rgba(176, 157, 203, 1)",
+                    borderWidth: 1,
+                    borderRadius: 3
+                },
+            ],
+        },
+        options: {
+            indexAxis: 'y',
+            plugins: {
+                legend: {
                     display: false,
                 }
             },
-            x: {
-                max: 100,
-                ticks: {
-                    font: {
-                        size: 10
+            scales: {
+                r: {
+                    ticks: {
+                        display: false,
+                    }
+                },
+                x: {
+                    max: 100,
+                    ticks: {
+                        font: {
+                            size: 10
+                        }
                     }
                 }
-            }
+            },
+            ticks: {
+                stepSize: 20,
+                beginAtZero: false,
+                font: {
+                    family: 'NanumSquareNeo-Variable',
+                    size: 12,
+                    weight: 'bold'
+                }
+            },
+            barThickness: 17,
         },
-        ticks: {
-            stepSize: 20,
-            beginAtZero: false,
-            font: {
-                family: 'NanumSquareNeo-Variable',
-                size: 12,
-                weight: 'bold'
-            }
-        },
-        barThickness: 17,
-    },
-});
-console.clear();
+    });
+}
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ prevBtn.addEventListener('click', (event) => {
     if (slideIndex < 0) {
         slideIndex = slides.children.length - 1;
     }
-    slides.style.transform = `translateX(-${slideIndex * 33.33}%)`;
+    slides.style.transform = `translateX(-${slideIndex * (100 / slides.children.length)}%)`;
 });
 
 nextBtn.addEventListener('click', (event) => {
@@ -18,21 +18,68 @@ nextBtn.addEventListener('click', (event) => {
     if (slideIndex > slides.children.length - 1) {
         slideIndex = 0;
     }
-    slides.style.transform = `translateX(-${slideIndex * 33.33}%)`;
+    slides.style.transform = `translateX(-${slideIndex * (100 / slides.children.length)}%)`;
 });
 
-// let intro = document.getElementById("intro");
-// let about = document.getElementById("about");
+var typingText1 = "지속적인 학습과 열정으로 성장하는 ".split('');
+var typingText2 = "백엔드 개발자,".split('');
+var typingText3 = "이은진".split('');
+var typingText4 = "입니다.".split('');
 
-// window.addEventListener("scroll", function() {
-//     let value = window.scrollY;
-//     console.log("scrollY", value);
+var span1 = document.getElementById('intro-text-1');
+var span2 = document.getElementById('intro-text-2');
+var span3 = document.getElementById('intro-text-3');
+var span4 = document.getElementById('intro-text-4');
 
-//     if (value < 400) {
-//         about.style.animation ='fade-out 0s ease-out forwards';
-//     } else if (value < 1300) {
-//         about.style.animation ='fade-in 1s ease-out';
-//     } else {
-//         about.style.animation ='fade-out 2s ease-out forwards';
-//     }
-// });
+var typingResult1 = "";
+var typingResult2 = "";
+var typingResult3 = "";
+var typingResult4 = "";
+
+function typingEffect1() {
+    if (typingText1.length > 0) {
+        typingResult1 += typingText1.shift();
+        span1.textContent = typingResult1;
+        setTimeout(typingEffect1, 70);
+    } else {
+        setTimeout(typingEffect2, 300);
+    }
+}
+
+function typingEffect2() {
+    if (typingText2.length > 0) {
+        typingResult2 += typingText2.shift();
+        span2.textContent = typingResult2;
+        setTimeout(typingEffect2, 70);
+    } else {
+        setTimeout(typingEffect3, 200);
+    }
+}
+
+function typingEffect3() {
+    if (typingText3.length > 0) {
+        typingResult3 += typingText3.shift();
+        span3.textContent = typingResult3;
+        setTimeout(typingEffect3, 70);
+    } else {
+        setTimeout(typingEffect4, 70);
+    }
+}
+
+function typingEffect4() {
+    if (typingText4.length > 0) {
+        typingResult4 += typingText4.shift();
+        span4.textContent = typingResult4;
+        setTimeout(typingEffect4, 70);
+    } else {
+        setTimeout(uncerlineDraw, 10);
+    }
+}
+
+const underline = document.getElementById("under-line");
+function uncerlineDraw() {
+    underline.style.animation = 'underline 1s ease-out forwards';
+}
+
+typingEffect1();
+
